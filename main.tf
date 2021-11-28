e853b10aefa539552d6dc4d7aefd9595bbfa8aaf
devhulk <gry2big93@gmail.com>
2021-11-28 16:05:34 -0500

    updated versions

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "networking" {
   source  = "app.terraform.io/gerald-tfc-business/networking/azure"
-  version = "0.0.3"
+  version = "0.0.5"
 
   environment = "${var.environment}"
   region = "${var.region}"
